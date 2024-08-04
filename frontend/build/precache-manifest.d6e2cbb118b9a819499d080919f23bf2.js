255bb604e0525479a619b232ccf771271b3331f7
saitejanannaka <saiteja.nannaka@phenom.com>
2024-08-04 17:31:24 +0530

    added

---
+++ frontend/build/precache-manifest.d6e2cbb118b9a819499d080919f23bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfbc0b2e7e8ac029cf12837ac0631df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b804200179a6487508",
+    "url": "/static/css/main.dcd519b1.chunk.css"
+  },
+  {
+    "revision": "059912caebcb2ab63ff9",
+    "url": "/static/js/2.9f4f23a7.chunk.js"
+  },
+  {
+    "revision": "06b804200179a6487508",
+    "url": "/static/js/main.19fa2775.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01d931191ed8e8736e2984dce9111336",
+    "url": "/static/media/bg.01d93119.jpg"
+  }
+]);
